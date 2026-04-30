d92491ecab2fd84177f3b36577ee512b61f4835b
Dakotah-Torres <dtorres@dtdevtech.com>
2026-04-30 18:15:12 -0500

    Setting up the project

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ git commit -m "$1"
 git push origin dev
 
 # 2. Merge into main
-git checkout main
+git checkout master
 git merge dev -m "Merge dev: $1"
-git push origin main
+git push origin master
 
 # 3. Back to dev
 git checkout dev
